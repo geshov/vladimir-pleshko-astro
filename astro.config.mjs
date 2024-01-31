c9f9a5fe287e5a10847cd4f02f778e338d165717
gesho <geshov@gmail.com>
2024-01-31 13:10:29 +0300

    add site url

--- astro.config.mjs
+++ astro.config.mjs
@@ -5,6 +5,7 @@ import alpinejs from "@astrojs/alpinejs";
 
 // https://astro.build/config
 export default defineConfig({
+  site: "https://pleshko.netlify.app/",
   integrations: [
     tailwind(),
     alpinejs({ entrypoint: "./src/assets/js/alpine" }),
