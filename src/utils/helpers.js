eb584bf48e8232fb00b304cb07453e11e0e27eda
gesho <geshov@gmail.com>
2024-02-01 18:55:05 +0300

    move history items to content

--- src/utils/helpers.js
+++ src/utils/helpers.js
@@ -1,7 +1,7 @@
 export const sortHistory = (items) => {
   items.sort((a, b) => {
-    if (a.data.year < b.data.year) return 1;
-    else if (a.data.year > b.data.year) return -1;
+    if (a.data.year > b.data.year) return 1;
+    else if (a.data.year < b.data.year) return -1;
     else return 0;
   });
   return items;
