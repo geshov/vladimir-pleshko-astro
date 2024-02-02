2b4bf4855d608f5036afd7212a3efb4d3032cee9
gesho <geshov@gmail.com>
2024-02-02 21:48:27 +0300

    optimisation

--- src/assets/js/competencies.js
+++ src/assets/js/competencies.js
@@ -2,7 +2,9 @@ import anime from "animejs/lib/anime.es.js";
 
 export default function () {
   return {
-    showCompetency(el) {
+    showCompetency() {
+      const el = this.$el;
+
       anime({
         targets: el,
         value: Number(el.dataset.value),
