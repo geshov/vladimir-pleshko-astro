0e66d17d8a8632dd0ffe480cd888f35bc6222d78
gesho <geshov@gmail.com>
2024-02-05 10:37:24 +0300

    add sitemap

--- astro.config.mjs
+++ astro.config.mjs
@@ -2,12 +2,16 @@ import { defineConfig } from "astro/config";
 
 import tailwind from "@astrojs/tailwind";
 import alpinejs from "@astrojs/alpinejs";
+import sitemap from "@astrojs/sitemap";
 
 // https://astro.build/config
 export default defineConfig({
   site: "https://pleshko.netlify.app/",
   integrations: [
     tailwind(),
-    alpinejs({ entrypoint: "./src/assets/js/alpine" }),
+    alpinejs({
+      entrypoint: "./src/assets/js/alpine",
+    }),
+    sitemap(),
   ],
 });
